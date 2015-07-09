5d35f09377171b8b46016c690a539a28ed466fe7
Phuoc Nguyen <tpthn@yahoo.com>
2015-07-09 16:51:55 -0700

    Initial Commit

---
+++ ViewControllerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ViewControllerDemo
+//
+//  Created by Phuoc Nguyen on 7/9/15.
+//  Copyright (c) 2015 PC Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ViewControllerDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ViewControllerDemo
+//
+//  Created by Phuoc Nguyen on 7/9/15.
+//  Copyright (c) 2015 PC Nguyen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+@end
